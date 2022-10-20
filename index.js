71f15140695b4d5d3d4d76ac373412a2b023960b
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-10-20 11:51:40 -0400

    added get all furniture and find furniture functions and paths

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express, { application } from "express";
-import { addNewFurniture } from "./src/furniture.js";
+import { addNewFurniture, getAllFurniture, findFurniture } from "./src/furniture.js";
 
 const app = express();
 const PORT = 3030;
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("63515b9a5491753c08398111");
-})
+app.get("/furniture", getAllFurniture);
 
 app.post("/furniture", addNewFurniture);
+
+app.get("/furniture/search/:key/:value", findFurniture);
 
 app.listen(PORT, ()=> console.log(`Listening on http://localhost:${PORT}...`))
--- src/furniture.js
+++ src/furniture.js
@@ -15,3 +15,35 @@ export async function addNewFurniture(req, res) {
 
     res.status(201).send(result);
 }
+
+export async function getAllFurniture (req,res){
+    const db = dbConnect();
+    const furnitureCollection = db.collection('furniture');
+    const result = await furnitureCollection.find({}).toArray()
+    .catch(err => {
+        res.status(500).send({success:false, message:err})
+        return
+    })
+    res.status(201).send({success:true, message: result})
+}
+
+export async function findFurniture(req,res){
+    const db = dbConnect();
+    const furnitureCollection = db.collection('furniture');
+
+    const { key, value } = req.params;
+    const search = new RegExp(value, "i");
+    const query = {
+    };
+    query[key] = { 
+        $regex: search
+    }
+
+    const result = await furnitureCollection.find(query).toArray()
+    .catch(err => {
+        res.status(500).send({success:false, message:err})
+        return
+    })
+
+    res.status(201).send({success:true, message: result})
+}
